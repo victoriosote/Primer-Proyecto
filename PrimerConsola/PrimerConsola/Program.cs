4b7d165265d0ea49b24d1d6331bb9efd0e661695
victoriosote <a2019112604@estudiantes.upsa.edu.bo>
2021-01-12 19:44:53 -0400

    primer git

---
+++ PrimerConsola/PrimerConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimerConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
