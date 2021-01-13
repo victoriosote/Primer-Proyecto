fdf38df350295c9d55c047fdf75c8cc3780d5ccb
victoriosote <a2019112604@estudiantes.upsa.edu.bo>
2021-01-12 20:59:14 -0400

    Saludo Personal

--- PrimerConsola/PrimerConsola/Program.cs
+++ PrimerConsola/PrimerConsola/Program.cs
@@ -6,7 +6,8 @@ namespace PrimerConsola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Buenas noches: Don Victor ");
         }
     }
 }
+  
